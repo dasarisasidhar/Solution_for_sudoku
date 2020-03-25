9408f8f8c03fd56bd97916a36109c654d99f9e23
Dasarraju Sasidhar <iad7kor@bosch.com>
2020-03-25 16:25:48 +0530

    added basic model

--- proj1.py
+++ proj1.py
@@ -1,17 +1,12 @@
 import numpy as np
 import time
-s = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]  
-    ]
-data = [1,2,3,4,5,6,7,8,9]
+
+def read():
+    with open("input/puzzle1.txt", "r") as file:
+        result = [[int(x) for x in line.split()] for line in file]
+        return result
+s = read()
+
 
 def find_possible_values(i,j, data):
     """
@@ -19,7 +14,6 @@ def find_possible_values(i,j, data):
         by substracting the valid row col and square values
 
     """
-
     a = {1,2,3,4,5,6,7,8,9}
     row = list()
     square = list()
@@ -78,92 +72,32 @@ def solve(arr):
     """
     start_time = time.time()
     arr = list(arr)
-    values = find_zeros_and_get_its_possible_values(s)
+    values = find_zeros_and_get_its_possible_values(arr)
+    ## s to arr
     for i in values:
         if(len(values[i]) == 1):
             value = values[i]
             j = int(i)
             arr[j//10][j%10] = int(value[0])
             solve(arr)
-    else:
-        print("Time taken to solve is %s seconds" % (time.time() - start_time))
-        print(arr)
-solve(s)
-
-#def test(i, j, data):
-#    print(data[i][j])
-#    square = list()
-#    x1 = i//3 *3
-#    y1 = (i//3 + 1) * 3
-#    x2 = j//3 * 3
-#    y2 = (j//3 + 1) * 3
-#    for i in range(9):
-#        for j in range(9):
-#            if(i>=x1 and i<y1  and j>=x2 and j<y2):
-#                square.extend(str(data[i][j]))
-
-#    print(square)
-
-#test(0,5,s)
-
-#def check_unique(value):
-#    flag = False
-#    unique = len(set(value)) == len(value)
-#    if(unique):
-#        print("unique")
-#    else:
-#         print("not unique")
+    
+    return(time.time() - start_time, arr)
 
 
+def write(time_, sol):
+    """ write the data to output file
+    """
+    with open('output/puzzle1.txt', 'w') as file:
+        file.write("Time taken to solve is: ")
+        file.write(str(time_))
+        file.write("possible Sol for given pazzle is: ")
+        file.write(str(sol))
 
-#def squares(data):
-#    square1 = list()
-#    square2 = list()
-#    square3 = list()
-#    square4 = list()
-#    square5 = list()
-#    square6 = list()
-#    square7 = list()
-#    square8 = list()
-#    square9 = list()
-#    for i in range(9):
-#        for j in range(9):
-#            if(i<3 and j<3):
-#                square1.extend(str(data[i][j]))
-#            if(i<3 and j>=3 and j<6):
-#                square2.extend(str(data[i][j]))
-#            if(i<3 and j>=6):
-#                square3.extend(str(data[i][j]))
-#            if(i>=3 and i<=6 and j<3):
-#                square4.extend(str(data[i][j]))
-#            if(i>=3 and i<=6  and j>=3 and j<6):
-#                square5.extend(str(data[i][j]))
-#            if(i>=3 and i<=6  and i<6 and j>=6):
-#                square6.extend(str(data[i][j]))
-#            if(i>=6 and j<3):
-#                square7.extend(str(data[i][j]))
-#            if(i>=6 and j>=3 and j<6):
-#                square8.extend(str(data[i][j]))
-#            if(i>=6  and j>=6):
-#                square9.extend(str(data[i][j]))
-    #print(square1, square2 , square3 , square4 , square5, square6 , square7 , square8 , square9)
 
-#def rows_cols(data):
-#    rows = list()
-#    cols = list()
-#    squares = list()
-#    for i in range(9):
-#        row = list(data[:][i])
-#        rows.append(row)
-#    #print("rows :", rows)
-#    for j in range(9):
-#        col = list()
-#        for k in range(9):
-#            col.append(data[k][j])
-#        cols.append(col)
-    #print("cols :", cols)
+data = solve(s)
+write(data[0], data[1])
 
-#rows_cols(s)
-#squares(s)
-#check_valid(data)
+print(data[0])
+print(data[1])
+
 
